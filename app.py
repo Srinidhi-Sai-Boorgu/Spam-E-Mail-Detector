a31c05d02370a4d0114065fba63182a54f0ba420
Srinidhi-Sai-Boorgu <142499467+Srinidhi-Sai-Boorgu@users.noreply.github.com>
2024-10-19 21:45:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import pickle
+
+with open('model.pkl', 'rb') as f:
+    model, vectorizer = pickle.load(f)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/detector', methods=['GET', 'POST'])
+def detector():
+    email_text = request.form['email_text']
+    processed_email_text = [' '.join(email_text.lower().split())]
+    text_vector = vectorizer.transform(processed_email_text)
+    result = model.predict(text_vector)[0]
+    return render_template('result.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
